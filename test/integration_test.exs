f832cfa21839c015cc3516236559c2a3527d190d
Brian Alexander <balexand@gmail.com>
2020-11-30 13:55:37 -0700

    fix failing tests when run concurrently

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -16,8 +16,13 @@ defmodule Sanity.MutateIntegrationTest do
 
     config = [dataset: "test", project_id: project_id, token: token]
 
+    recently = DateTime.utc_now() |> DateTime.add(-60, :second) |> DateTime.to_iso8601()
+
     Sanity.mutate([
-      %{delete: %{query: ~S<*[_type in ["sanity.imageAsset", "product"]]>}}
+      %{delete: %{query: ~s<*[
+          _type in ["sanity.imageAsset", "product"] &&
+          dateTime(_createdAt) < dateTime(#{recently})")
+        ]>}}
     ])
     |> Sanity.request(config)
 
