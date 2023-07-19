e9fe0d38bb01493aa2bca0d434e9789575bd4605
Brian Alexander <balexand@gmail.com>
2023-07-19 16:10:35 -0600

    Update listener.ex

--- lib/sanity/listener.ex
+++ lib/sanity/listener.ex
@@ -1,5 +1,7 @@
 defmodule Sanity.Listener do
-  alias Sanity.Request
+  defmodule Event do
+    defstruct data: nil, event: nil, id: nil
+  end
 
   @opts_schema [
     api_version: [
@@ -10,6 +12,15 @@ defmodule Sanity.Listener do
       type: :string,
       doc: "Sanity dataset.",
       required: true
+    ],
+    project_id: [
+      type: :string,
+      doc: "Sanity project ID.",
+      required: true
+    ],
+    query_params: [
+      type: :keyword_list,
+      default: []
     ],
     variables: [
       type: :map,
@@ -23,15 +34,76 @@ defmodule Sanity.Listener do
 
   def start_link(query, opts) do
     opts = NimbleOptions.validate!(opts, @opts_schema)
+    query_params = Sanity.query_to_query_params(query, opts[:variables], opts[:query_params])
 
-    _url =
-      "https://api.sanity.io/#{opts[:api_version]}/data/listen/#{opts[:dataset]}?#{URI.encode_query(query_params)}"
+    url =
+      "https://#{opts[:project_id]}.api.sanity.io/#{opts[:api_version]}/data/listen/#{opts[:dataset]}?#{URI.encode_query(query_params)}"
 
-    headers(opts)
+    request = Finch.build(:get, url, headers(opts))
+    pid = self()
 
     spawn_link(fn ->
-      nil
+      Finch.stream(
+        request,
+        Sanity.Finch,
+        "",
+        fn
+          {:status, 200}, acc ->
+            acc
+
+          {:status, status}, _acc ->
+            raise "response error status #{inspect(status)}"
+
+          {:headers, _headers}, acc ->
+            acc
+
+          {:data, data}, acc ->
+            process_data(acc <> data, pid)
+        end,
+        receive_timeout: 60_000
+      )
     end)
+  end
+
+  defp process_data(data, pid) do
+    case String.split(data, "\n\n", parts: 2) do
+      [payload, rest] ->
+        payload |> String.trim() |> process_payload(pid)
+        process_data(rest, pid)
+
+      [rest] ->
+        rest
+    end
+  end
+
+  defp process_payload(":", _pid), do: nil
+
+  defp process_payload(payload, pid) do
+    map =
+      payload
+      |> String.split("\n")
+      |> Map.new(fn line ->
+        [key, value] = String.split(line, ": ", parts: 2)
+        {key, value}
+      end)
+
+    process_event(
+      %Event{
+        data: map["data"] && Jason.decode!(map["data"]),
+        event: map["event"],
+        id: map["id"]
+      },
+      pid
+    )
+  end
+
+  defp process_event(%Event{event: event_name} = event, _pid)
+       when event_name in ~W[channelError disconnect] do
+    raise "error event #{inspect(event)}"
+  end
+
+  defp process_event(%Event{} = event, pid) do
+    send(pid, event)
   end
 
   defp headers(opts) do
